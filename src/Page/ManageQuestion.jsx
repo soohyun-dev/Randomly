d8a551f04613503a2a9c35593e2fa13d25137f6c
soohyun <bmy1320@gmail.com>
2023-01-30 14:51:52 +0900

    feat: 등록된 질문 리스트 오름차순으로 정렬
    close #14

--- src/Page/ManageQuestion.jsx
+++ src/Page/ManageQuestion.jsx
@@ -1,6 +1,12 @@
-import { useEffect, useId, useState } from "react";
+import { useEffect, useState } from "react";
 import { fireStore } from "../firebase";
-import { addDoc, collection, getDoc, getDocs, doc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import styled from "styled-components";
 import Nav from "../Components/Nav";
 import QuestionTable from "../Components/Interview/QuestionTable";
@@ -13,7 +19,7 @@ export default function ManageQuestion() {
   const questionInfo = collection(fireStore, "questions");
 
   const getQuestions = async () => {
-    const data = await getDocs(questionInfo);
+    const data = await getDocs(query(questionInfo, orderBy("time", "asc")));
     questions.current = data.docs.map((doc) => ({
       ...doc.data(),
       id: doc.id,
@@ -28,12 +34,14 @@ export default function ManageQuestion() {
    * @param {Number}
    */
   const addQuestion = async () => {
-    const idx = Object.keys(questions).length;
+    const idx = Object.keys(questions.current).length;
     const newData = {};
     newData["idx"] = idx;
     newData["question"] = newQuestion;
+    newData["time"] = new Date();
     await addDoc(questionInfo, newData);
     alert("질문이 추가되었습니다.");
+    getQuestions();
     window.location.reload();
   };
 
@@ -44,33 +52,35 @@ export default function ManageQuestion() {
   return (
     <>
       <Nav />
-      <div>질문관리</div>
       <div>
-        <input
-          type="text"
-          placeholder="추가할 질문을 입력해주세요."
-          onChange={(e) => {
-            setNewQuestion(e.target.value);
-          }}
-        />
-        <button onClick={addQuestion}>질문 추가</button>
-      </div>
-      <div>
-        <table border="1">
-          <th>No.</th>
-          <th>질문</th>
-          <th>수정하기</th>
-          <th>삭제하기</th>
-          {show
-            ? Object.keys(questions.current).map((v, idx) => (
-                <QuestionTable
-                  question={questions.current[~~v].question}
-                  id={questions.current[~~v].id}
-                  idx={idx}
-                />
-              ))
-            : ""}
-        </table>
+        <p>질문관리</p>
+        <div>
+          <input
+            type="text"
+            placeholder="추가할 질문을 입력해주세요."
+            onChange={(e) => {
+              setNewQuestion(e.target.value);
+            }}
+          />
+          <button onClick={addQuestion}>질문 추가</button>
+        </div>
+        <div>
+          <table border="1">
+            <th>No.</th>
+            <th>질문</th>
+            <th>수정하기</th>
+            <th>삭제하기</th>
+            {show
+              ? Object.keys(questions.current).map((v, idx) => (
+                  <QuestionTable
+                    question={questions.current[~~v].question}
+                    id={questions.current[~~v].id}
+                    idx={idx}
+                  />
+                ))
+              : ""}
+          </table>
+        </div>
       </div>
     </>
   );
