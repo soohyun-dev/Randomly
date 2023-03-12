063f67c655677c75cd69ae793762172a7d67d664
soohyun <bmy1320@gmail.com>
2023-03-12 13:56:55 +0900

    feat: 카테고리 추가 버튼 기능 수정 및 스타일 변경
    close #107

--- src/Components/ManageInterview/Catagory/index.tsx
+++ src/Components/ManageInterview/Catagory/index.tsx
@@ -1,25 +1,22 @@
 import { chooseFolder, chooseId, selectFolder } from 'features/folderSlice'
-import { questionsSlice, selectCatagoryList, selectSelectedCatagory } from 'features/questionsSlice'
-import { fireStore } from '../../../firebase'
-import { collection, doc, getDocs, orderBy, query, updateDoc } from 'firebase/firestore'
+import { questionsSlice, selectSelectedCatagory } from 'features/questionsSlice'
+import { doc, updateDoc } from 'firebase/firestore'
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { CatagoryBox } from './styles'
 import { selectUser } from 'features/userSlice'
-
-const log = console.log
+import { CatagoryBox, CatagoryButton, NewCatagoryInput, PlusCatagoryButton } from './styles'
+import { fireStore } from '../../../firebase'
 
 export default function Catagory() {
     const user = useSelector(selectUser)
     const folderId = useSelector(chooseId)
     const folders = useSelector(selectFolder)
+    const chooseCatagory = useSelector(selectSelectedCatagory)
     const choose = useSelector(chooseFolder)
-    const catago = useSelector(selectSelectedCatagory)
-    const CatagoryList = useSelector(selectCatagoryList)
-    const catagoryInfo = collection(fireStore, `users/${user}/packages/${folderId}/catagory`)
     const dispatch = useDispatch()
     const [catagory, setCatagory] = useState(folders[choose].catagory)
     const [newCatagory, setNewCatagory] = useState('')
+    const [chooseBtn, setChooseBtn] = useState(false)
 
     const clickCatagory = (e) => {
         dispatch(
@@ -37,6 +34,30 @@ export default function Catagory() {
         alert('카테고리가 추가되었습니다.')
     }
 
+    const renderBtn = () => {
+        if (catagory.length >= 5) {
+            return ''
+        }
+        if (chooseBtn) {
+            return (
+                <div>
+                    <NewCatagoryInput
+                        type="text"
+                        value={newCatagory}
+                        placeholder="추가할 질문을 입력해주세요."
+                        onChange={(e) => setNewCatagory(e.target.value)}
+                    />
+                    <PlusCatagoryButton onClick={addCatagory}>추가</PlusCatagoryButton>
+                    <PlusCatagoryButton onClick={() => setChooseBtn(false)}>
+                        {' '}
+                        취소{' '}
+                    </PlusCatagoryButton>
+                </div>
+            )
+        }
+        return <PlusCatagoryButton onClick={() => setChooseBtn(true)}> + </PlusCatagoryButton>
+    }
+
     useEffect(() => {
         setCatagory(folders[choose].catagory)
         dispatch(
@@ -52,23 +73,18 @@ export default function Catagory() {
     }, [folders, newCatagory])
 
     return (
-        <>
-            <CatagoryBox>
-                <select id="Catagory" onChange={clickCatagory}>
-                    {folders[choose].catagory.map((title) => (
-                        <option value={title}>{title}</option>
-                    ))}
-                </select>
-                <div>
-                    <input
-                        type="text"
-                        value={newCatagory}
-                        placeholder="추가할 질문을 입력해주세요."
-                        onChange={(e) => setNewCatagory(e.target.value)}
-                    />
-                    <button onClick={addCatagory}>카테고리 추가</button>
-                </div>
-            </CatagoryBox>
-        </>
+        <CatagoryBox>
+            {folders[choose].catagory.map((title) => (
+                <CatagoryButton
+                    value={title}
+                    select={title === chooseCatagory ? 'choose' : 'no'}
+                    onClick={clickCatagory}
+                >
+                    {title}
+                </CatagoryButton>
+            ))}
+
+            {renderBtn()}
+        </CatagoryBox>
     )
 }
--- src/Components/ManageInterview/Catagory/styles.tsx
+++ src/Components/ManageInterview/Catagory/styles.tsx
@@ -1,3 +1,41 @@
 import styled from 'styled-components'
 
-export const CatagoryBox = styled.div``
+interface Choose {
+    select?: string
+}
+
+export const CatagoryBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const CatagoryButton = styled.button<Choose>`
+    height: 2em;
+    padding: 0 1em;
+    border: none;
+    border-radius: 10px;
+    margin: 1em 1em;
+    color: ${(props) => (props.select === 'choose' ? 'white' : '')};
+    background-color: ${(props) => (props.select === 'choose' ? 'gray' : '')};
+    cursor: pointer;
+    &:hover {
+        opacity: 0.9;
+    }
+`
+
+export const NewCatagoryInput = styled.input`
+    border: solid 0.5px gray;
+    width: 20em;
+    height: 2em;
+    border-radius: 5px;
+`
+
+export const PlusCatagoryButton = styled.button`
+    margin: 0 0.5em;
+    border: none;
+    cursor: pointer;
+    &:hover {
+        opacity: 0.9;
+    }
+`
